dc24052072859be5ab11dd42cd8b10187372f072
shinnguyen0105 <nhatncgcd17202@fpt.edu.vn>
2021-01-12 22:54:04 +0700

    add song function done!

--- Checkpoint/checkpointmiddle/src/pages/Add.js
+++ Checkpoint/checkpointmiddle/src/pages/Add.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-
+import React, { useState } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import songList from '../assets/songs';
+import { useHistory } from 'react-router-dom';
 const Add = () => {
+  const initialState = {
+    namesong: '',
+    author: '',
+    lyrics: '',
+  };
+  const [input, setInput] = useState(initialState);
+  const history = useHistory();
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setInput((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    let crypto = require('crypto');
+    let id = crypto.randomBytes(16).toString('hex');
+    let publishDate = new Date().toLocaleString();
+    songList.push({
+      id: id,
+      author: input.author,
+      publishedDate: publishDate,
+      title: input.namesong,
+      lyric: input.lyrics,
+    });
+    alert('Successful add song: ' + input.namesong);
+    history.push(`/songs/${id}`);
+  };
   return (
     <div>
-      <Form>
+      <Form onSubmit={HandleSubmit}>
         <FormGroup>
           <Label for='name'>Name Song</Label>
           <Input
@@ -12,6 +39,7 @@ const Add = () => {
             name='namesong'
             id='namesong'
             placeholder='Name Song'
+            onChange={onChangeInput}
           />
         </FormGroup>
         <FormGroup>
@@ -21,11 +49,18 @@ const Add = () => {
             name='author'
             id='nameauthor'
             placeholder='Author'
+            onChange={onChangeInput}
           />
         </FormGroup>
         <FormGroup>
           <Label for='lyrics'>Lyrics</Label>
-          <Input type='lyrics' name='lyrics' id='lyrics' placeholder='lyrics' />
+          <Input
+            type='name'
+            name='lyrics'
+            id='lyrics'
+            placeholder='lyrics'
+            onChange={onChangeInput}
+          />
         </FormGroup>
         <Button>Submit</Button>
       </Form>
